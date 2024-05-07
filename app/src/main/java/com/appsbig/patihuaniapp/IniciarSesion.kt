06e8a08d6d116c4498c8b5323a2487d7986013ff
DavidGJ2002 <dgrandej2000@alumno.ipn.mx>
2024-05-07 09:39:15 -0600

    Cambios en interfaz e implemetación de código para el login y registrar

--- app/src/main/java/com/appsbig/patihuaniapp/IniciarSesion.kt
+++ app/src/main/java/com/appsbig/patihuaniapp/IniciarSesion.kt
@@ -1,39 +1,45 @@
-    package com.appsbig.patihuaniapp
+package com.appsbig.patihuaniapp
 
-    import android.content.Intent
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.widget.Button
-    import android.widget.EditText
-    import android.widget.TextView
-    import android.widget.Toast
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.*
 
-    class IniciarSesion : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_iniciarsesion)
+class IniciarSesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_iniciarsesion)
 
-            val btnSiguiente = findViewById<Button>(R.id.botonIS)
-            val ptUsuario = findViewById<EditText>(R.id.nombre)
-            val ptContrasena = findViewById<EditText>(R.id.contrasena)
-            val opRegistrar = findViewById<TextView>(R.id.opcionRegistrate)
+        val btnSiguiente = findViewById<Button>(R.id.botonIS)
+        val ptUsuario = findViewById<EditText>(R.id.nombre)
+        val ptContrasena = findViewById<EditText>(R.id.contrasena)
+        val opRegistrar = findViewById<TextView>(R.id.opcionRegistrate)
 
-            btnSiguiente.setOnClickListener {
-                val usuario = ptUsuario.text.toString()
-                val contrasena = ptContrasena.text.toString()
+        val database = FirebaseDatabase.getInstance()
+        val usuariosRef = database.getReference("usuarios")
 
-                if (usuario == "david" && contrasena == "123") {
-                    val intent = Intent(this, Bienvenido::class.java)
-                    intent.putExtra("usuario", usuario)
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show()
-                }
-            }
+        btnSiguiente.setOnClickListener {
+            val usuario =ptUsuario.text.toString()
+            val contrasena = ptContrasena.text.toString()
 
-            opRegistrar.setOnClickListener {
-                val intentRegistraUs = Intent(this,Registrar::class.java)
-                startActivity(intentRegistraUs)
+            if(usuario == "david" && contrasena == "123"){
+                val intent = Intent(this, Bienvenido::class.java)
+                intent.putExtra("usuario", usuario)
+
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Datos incorrectos", Toast.LENGTH_SHORT).show()
             }
         }
+
+        opRegistrar.setOnClickListener {
+            val intentRegistraUs = Intent(this, Registrar::class.java)
+            startActivity(intentRegistraUs)
+        }
     }
+}
+
