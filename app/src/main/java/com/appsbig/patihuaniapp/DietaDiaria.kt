130c8c5c21f62b64d8e3a5fdd0b89d41bded7f78
ALVARO YAIR GARCIA MURRIETA <zS20018183@estudiantes.uv.mx>
2024-05-07 10:29:09 -0600

    mis pantallas

---
+++ app/src/main/java/com/appsbig/patihuaniapp/DietaDiaria.kt
@@ -0,0 +1,11 @@
+package com.appsbig.patihuaniapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DietaDiaria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dieta_diaria)
+    }
+}
---
+++ app/src/main/java/com/appsbig/patihuaniapp/Metas.kt
@@ -0,0 +1,11 @@
+package com.appsbig.patihuaniapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Metas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_metas)
+    }
+}
