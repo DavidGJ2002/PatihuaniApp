faaacaa14e53be8aa47ea6df1821c7e2f8d2d0ad
juanCarlosGarciaLDG <jucaga100@gmail.com>
2024-05-07 23:55:36 -0600

    subo mis activitis

---
+++ app/src/main/java/com/appsbig/patihuaniapp/alternativasDietas.kt
@@ -0,0 +1,20 @@
+package com.appsbig.patihuaniapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class alternativasDietas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_alternativas_dietas)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
